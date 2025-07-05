3486bb10084a17cb922d649535d4531202d0f8db
Piotr Buczkowski <p.d.buczkowski@gmail.com>
2025-07-05 20:57:44 +0200

    using the 'given-perform-expect' dependency from jitpack.io;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    testImplementation(project(":given-perform-expect"))
+    testImplementation(libs.given.perform.expect)
     testImplementation(libs.mockk)
     testImplementation(libs.kotlin.test)
     testImplementation(libs.junit)
