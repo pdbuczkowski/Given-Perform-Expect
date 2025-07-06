c9cc129a3d04d4a87ba563057850ab24b8b55dac
Piotr Buczkowski <p.d.buczkowski@gmail.com>
2025-07-06 15:13:51 +0200

    moving the sample data to the 'main';

---
+++ app/src/main/java/com/example/myapplication/SampleData.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+class MyException1: Exception()
+class MyException2: Exception()
+
+class MyClass(
+    var itsState: InternalState
+) {
+    fun changeState(anotherState: InternalState) { itsState = anotherState }
+    fun someAction() {
+        itsState.internalStateAction()
+        itsState.anotherAction()
+    }
+}
+
+class InternalState {
+    fun internalStateAction() {}
+    fun anotherAction() {}
+}
+
+class ClassWithInt(val internalState: InternalState = InternalState()) {
+    var int: Int? = null
+
+    fun initState(value: Int = 0) { int = value }
+
+    fun signalException() {
+        internalState.internalStateAction()
+        throw MyException2()
+    }
+}
--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -100,32 +100,3 @@ class ExampleUnitTest {
         },
     )
 }
-
-private class MyException1: Exception()
-private class MyException2: Exception()
-
-class MyClass(
-    var itsState: InternalState
-) {
-    fun changeState(anotherState: InternalState) { itsState = anotherState }
-    fun someAction() {
-        itsState.internalStateAction()
-        itsState.anotherAction()
-    }
-}
-
-class InternalState {
-    fun internalStateAction() {}
-    fun anotherAction() {}
-}
-
-class ClassWithInt(val internalState: InternalState = InternalState()) {
-    var int: Int? = null
-
-    fun initState(value: Int = 0) { int = value }
-
-    fun signalException() {
-        internalState.internalStateAction()
-        throw MyException2()
-    }
-}
